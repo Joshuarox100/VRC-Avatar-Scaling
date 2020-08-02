d3a8862d33f1557a8fb52a03833fe8412c4c3efa
Joshuarox100 <joshuabender10@gmail.com>
2020-08-02 18:09:25 -0500

    changed tooltip

--- Editor/AvatarScalingWindow.cs
+++ Editor/AvatarScalingWindow.cs
@@ -169,7 +169,7 @@ public class AvatarScalingWindow : EditorWindow
         manager.expressionsMenu = (VRCExpressionsMenu)EditorGUILayout.ObjectField(new GUIContent("Expressions Menu", "(Optional) The Expressions Menu you want the scaling controls added to. Leave this empty if you don't want any menus to be affected.\n(Controls will be added as a submenu.)"), manager.expressionsMenu, typeof(VRCExpressionsMenu), true);
         EditorGUILayout.Space();
         GUILayout.BeginHorizontal();
-        GUILayout.Label(new GUIContent("Add Parameters", "Check the needed parameters for scaling within the Avatar's Expression Parameters and add them if they're absent."), GUILayout.Width(145));
+        GUILayout.Label(new GUIContent("Add Parameters", "Check for needed parameters within the Avatar's Expression Parameters and add them if necessary."), GUILayout.Width(145));
         manager.addExpressionParameters = Convert.ToBoolean(GUILayout.Toolbar(Convert.ToInt32(manager.addExpressionParameters), new string[] { "No", "Yes" }));
         GUILayout.EndHorizontal();
         EditorGUILayout.Space();
